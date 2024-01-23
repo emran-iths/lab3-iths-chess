5e7daee695d85626a9e527a23a8f9e13e83f92a1
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-23 18:48:28 +0100

    resolve update issue. added key to log list

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,44 @@ import { useState, useEffect } from "react";
 
 let gameId = "r5vTXSNZs73N";
 
+let preparedGames = new Set();
+let gameListeners = new Set();
+
+const prepareGame = (gameId) => {
+  if (preparedGames.has(gameId)) return;
+  preparedGames.add(gameId);
+
+  console.log(`connects to game ${gameId}`);
+  gameStream(gameId, (data) => {
+    gameListeners.forEach((f) => {
+      f(data);
+    });
+
+    gameListeners.clear();
+  });
+};
+
+const listenToGame = (callback) => {
+  gameListeners.add(callback);
+};
+
 export default function Home() {
   const [log, setLog] = useState([]);
   useEffect(() => {
-    gameStream(gameId, (data) => {
+    prepareGame(gameId);
+  }, []);
+
+  useEffect(() => {
+    listenToGame((data) => {
       console.log(data);
       setLog(log.concat(data));
     });
-  }, []);
+  }, [log]);
 
   return (
     <ul>
-      {log.map((c) => (
-        <li>{c}</li>
+      {log.map((c, i) => (
+        <li key={i}>{c}</li>
       ))}
     </ul>
   );
