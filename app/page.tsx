a3ac6cd6b8f4b1d261414ac51e5137038e9add73
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-24 12:11:26 +0100

    added start game button

--- app/page.tsx
+++ app/page.tsx
@@ -73,6 +73,24 @@ export default function Home() {
       >
         send
       </button>
+
+      <button
+        onClick={() => {
+          console.log("click");
+          const url = `https://lichess.org/api/challenge/ai`;
+          fetch(url, {
+            method: "post",
+            body: "level=7&color=white",
+
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              Authorization: `Bearer ${process.env.NEXT_PUBLIC_LICHESS_TOKEN}`,
+            },
+          });
+        }}
+      >
+        start
+      </button>
     </>
   );
 }
