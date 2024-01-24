377b6e8a9341520c8df512ff980830352332663c
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-24 16:34:41 +0100

    removed everything but start button on index page since we seperated game from index

--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,10 @@
 "use client";
 
 import styles from "./page.module.css";
-import gameStream from "./GameStream";
-import { useState, useEffect } from "react";
-
-let gameId = "r5vTXSNZs73N";
-
-let preparedGames = new Set();
-let gameListeners: { [id: string]: Set<any> } = {};
-
-const prepareGame = (gameId: string) => {
-  if (preparedGames.has(gameId)) return;
-  preparedGames.add(gameId);
-  gameListeners[gameId] = new Set();
-
-  console.log(`connects to game ${gameId}`);
-  gameStream(gameId, (data) => {
-    gameListeners[gameId].forEach((f: any) => {
-      f(data);
-    });
-
-    gameListeners[gameId].clear();
-  });
-};
-
-const listenToGame = (callback: any) => {
-  if (!(gameId in gameListeners)) return;
-  gameListeners[gameId].add(callback);
-};
 
 export default function Home() {
-  const [log, setLog] = useState([]);
-  useEffect(() => {
-    prepareGame(gameId);
-  }, []);
-
-  useEffect(() => {
-    listenToGame((data: any) => {
-      console.log(data);
-      setLog(log.concat(data));
-    });
-  }, [log]);
-
-  const [move, setMove] = useState("");
-
   return (
     <>
-      <ul>
-        {log.map((c, i) => (
-          <li key={i}>{c}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        value={move}
-        onChange={(e) => {
-          setMove(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          const url = `https://lichess.org/api/bot/game/${gameId}/move/${move}`;
-          console.log(url);
-          fetch(url, {
-            method: "post",
-
-            headers: {
-              Authorization: `Bearer ${process.env.NEXT_PUBLIC_LICHESS_TOKEN}`,
-            },
-          });
-        }}
-      >
-        send
-      </button>
-
       <button
         onClick={() => {
           console.log("click");
