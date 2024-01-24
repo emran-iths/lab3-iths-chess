24b1af67b0e6f33b78786bdf6c0932c8d700a454
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-24 16:42:36 +0100

    after started game it now shows a link to that new game

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import styles from "./page.module.css";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [startedGame, setStartedGame] = useState(null);
+
   return (
     <>
       <button
         onClick={() => {
-          console.log("click");
           const url = `https://lichess.org/api/challenge/ai`;
           fetch(url, {
             method: "post",
@@ -17,11 +20,17 @@ export default function Home() {
               "Content-Type": "application/x-www-form-urlencoded",
               Authorization: `Bearer ${process.env.NEXT_PUBLIC_LICHESS_TOKEN}`,
             },
-          });
+          })
+            .then((res) => res.json())
+            .then((data) => setStartedGame(data.id));
         }}
       >
         start
       </button>
+
+      {startedGame && (
+        <Link href={`/game?gameId=${startedGame}`}>Goto game</Link>
+      )}
     </>
   );
 }
