97fe9440c4dc6a36a225b5c339bc2979be5d1edb
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-27 22:58:37 +0100

    added chess board (and its error coundary component and init state). added test for chess board

---
+++ __tests__/ChessBoard.test.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import ChessBoard from "../app/components/ChessBoard";
+
+describe("ChessBoard", () => {
+  it("render a chess board without getting any errors", () => {
+    render(<ChessBoard onClickSquare={() => {}} />);
+  });
+});
