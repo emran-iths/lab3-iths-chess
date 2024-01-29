b683d9f15b8cdb7a570e2609d9486f543b79359e
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-29 22:39:25 +0100

    added description for play chess

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -25,6 +25,18 @@ export default function Home() {
       </ul>
       <hr />
       <div>
+        <h2>Play chess</h2>
+        <p>
+          Press the start button to start a new game agaist a computer player.
+          You are always white (and you play agaist a level 7 lichess bot (good
+          luck!)). Press on the square you want to move from and the square you
+          want to move to and press "send". Note the input field is your move so
+          manually enter and clear it when you need to. Do not expect this
+          lichess client to be any good but it works if you know what you are
+          doing. The client does not implement upgrade your pawns so for that
+          you need to enter that move manually, ex: a7a8q (q = queen) it should
+          work but the piece will still look like a pawn.
+        </p>
         <StartButton />
       </div>
     </>
